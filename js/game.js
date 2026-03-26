8f753461bace5cfbfcaefafced102bb4f5a74d75
adsimisterra-spec <adsimisterra@est.istg.edu.ec>
2026-03-26 13:01:25 -0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -9,4 +9,4 @@ const MADRINA_VALUES = [150, 200, 250, 300, 350, 400, 450, 500];
 const BALL_RADIUS = 12;
 const PLAYER_SPEED = 4;
 const THROW_FORCE = 1.2; // Factor de fuerza del lanzamiento
-const INVINCIBILITY_DURATION = 3000;
+
